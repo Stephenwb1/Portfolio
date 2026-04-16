f77b32dcab5bc71d16114920252b7d90212443fe
stephenwb1 <stephenwb360@gmail.com>
2026-04-16 15:07:34 -0700

    added tcp app

--- src/data/projects.js
+++ src/data/projects.js
@@ -8,12 +8,12 @@ export const projects = [
         description: "Open-source AI-assisted\ncodebase comprehension\nand analysis tool"
     },
     {
-        title: "CodeCompass",
-        url: "https://codecompass-eight.vercel.app/",
+        title: "TCP chat app",
+        url: "https://github.com/Stephenwb1/TCP-Chat-Application",
         github: "Github",
-        github_url: "https://github.com/vinngo/codecompass",
-        youtubeId: "dQw4w9WgXcQ",
-        description: "Open-source AI-assisted codebase comprehension\nand analysis tool"
+        github_url: "https://github.com/Stephenwb1/TCP-Chat-Application",
+        youtubeId: "xi1dNlnWOjE",
+        description: "A multi-user real-time chat application\nbuilt on TCP sockets in Python\n\nImplemented a client-server architecture handling\nconcurrent connections and message routing\nbetween multiple users\n\nDeployed and tested within a Mininet virtual network\nenvironment, simulating real-world network\ntopology and conditions"
     },
     {
         title: "CodeCompass",
